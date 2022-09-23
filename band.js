2ff2456b391a2bd5a8a762bff0a370bd9366c7a8
folatheman <folajunior1@gmail.com>
2022-09-22 23:46:47 -0400

    new

--- band.js
+++ band.js
@@ -3,13 +3,11 @@ function validateform()
     const text = document.getElementById('text').value;
 const password =document.getElementById('password').value;
 
-if(text==null || text==''){
-    alert('Enter login information');
-    return false
-}
+if(text=="Julie" && password=="tania"){
+    alert("logged in");
 
-if(password.length<=6){
-    alert('Password incorrect');
-    return false
+    window.location ="home.html"
+} else{
+    alert("Incorrect username or password")
 }
 }
