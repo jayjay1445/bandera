ecf5be5ce84a58222e82f1eb10c03f57a56668d8
folatheman <folajunior1@gmail.com>
2022-09-23 12:31:49 -0400

    last

--- band.js
+++ band.js
@@ -5,7 +5,7 @@ var password =document.getElementById('password').value;
 
 if(text=="Julie" && password=="tania"){
 
-    window.location ="home.html"
+    window.location.href="https://github.com/jayjay1445/bandera/blob/76103e281da9cb7a07d0358670378d1e8ec115ba/home.html"
 } else{
     alert("Incorrect username or password")
 }
