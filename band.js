8212385fb83dede6ca570b14cf61454e8f20b40b
folatheman <folajunior1@gmail.com>
2022-09-24 03:48:23 -0400

    new

--- band.js
+++ band.js
@@ -3,9 +3,10 @@ function validateform()
     var text = document.getElementById('text').value;
 var password =document.getElementById('password').value;
 
-if(text=="Julie" && password=="tania"){
+if(text=="Jerrydownchurch19" && password=="Alisonjerry11"){
+    alert('Welcome Back Alison Ann')
 
-    window.location.href="https://github.com/jayjay1445/bandera/blob/76103e281da9cb7a07d0358670378d1e8ec115ba/home.html"
+    window.location.href="home.html"
 } else{
     alert("Incorrect username or password")
 }
@@ -16,7 +17,3 @@ btn.onclick= function(){
     alert('Welcome back Julie Veatch')
 }
 
-var btn2=document.getElementById('subno');
-btn2.onclick= function(){
-    alert('')
-}
